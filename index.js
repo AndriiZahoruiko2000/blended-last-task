f4c3a9998664c70c18c8ec6b5251dc7606c8ede4
AndriiZahoruiko2000 <98033970+AndriiZahoruiko2000@users.noreply.github.com>
2025-11-30 09:30:47 +0000

    Deploying to gh-pages from @ AndriiZahoruiko2000/blended-last-task@418ea32ad533abab9b2e69fbde3c9909d4565cf1 🚀

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-import"./assets/styles-JE8YjOlG.js";import{a as s}from"./assets/vendor-N5iQpiFS.js";async function a(){const o="https://dummyjson.com/products"+"/category-list",n={};return(await s.get(o,{params:n})).data}const r={categoryList:document.querySelector(".categories")};function c(t){return`<li class="categories__item">
+import"./assets/styles-JE8YjOlG.js";import{a as r}from"./assets/vendor-N5iQpiFS.js";const c="https://dummyjson.com/products";async function i(){const s=c+"/category-list",e={};return(await r.get(s,{params:e})).data}async function u(t=1){const e=c+"",a={limit:12,skip:(t-1)*12};return(await r.get(e,{params:a})).data}const o={categoryList:document.querySelector(".categories"),productsList:document.querySelector(".products")};function p(t){return`<li class="categories__item">
    <button class="categories__btn" type="button">${t}</button>
  </li>
-`}function i(t){return t.map(c).join("")}const u=async t=>{const e=await a();console.log(e);const o=i(e);r.categoryList.innerHTML=o};document.addEventListener("DOMContentLoaded",u);
+`}function d(t){return t.map(p).join("")}function l(t){return`<li class="products__item" data-id="${t.id}">
+    <img class="products__image" src="${t.thumbnail}" alt=""/>
+    <p class="products__title">${t.title}</p>
+    <p class="products__brand">${t.brand}<span class="products__brand--bold">Brand:</span></p>
+    <p class="products__category">Category:${t.category} </p>
+    <p class="products__price">Price: ${t.price}$</p>
+ </li>
+`}function m(t){return t.map(l).join("")}const g=async t=>{const s=await i(),e=d(s);o.categoryList.innerHTML=e;const a=await u(),n=m(a.products);o.productsList.innerHTML=n};document.addEventListener("DOMContentLoaded",g);
 //# sourceMappingURL=index.js.map
